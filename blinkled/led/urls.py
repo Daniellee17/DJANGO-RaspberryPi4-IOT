1cf2365753dc6e449f6a1e8f942d9285b3e7cbc3
Daniellee17 <Danielleespiritu17@gmail.com>
2019-03-26 23:55:41 +0800

    1

---
+++ blinkled/led/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+from . import views as led_views
+
+
+
+urlpatterns = [
+    path('', led_views.main, name = "home"),
+        
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
