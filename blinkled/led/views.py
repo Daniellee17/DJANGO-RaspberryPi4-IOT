b9c4fe11724d8b3bd64c8b74642a81e7a5a31aa6
Danielle Marie Espiritu <danielleespiritu17@gmail.com>
2019-04-01 18:49:24 +0800

    1

--- blinkled/led/views.py
+++ blinkled/led/views.py
@@ -2,19 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+# Create your views here.
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT, initial=0)
+GPIO.setup(19, GPIO.OUT, initial=0)
+sensor = Adafruit_DHT.DHT11
 
 def main(request):
     if (request.GET.get('on_btn')):
         print("YOU HAVE TURNED IT ON")
+        GPIO.output(26, GPIO.HIGH)
+        GPIO.output(19, GPIO.LOW)
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, 13)
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+
+
 
     if (request.GET.get('off_btn')):
         print("YOU HAVE TURNED IT OFF")
+        GPIO.output(26, GPIO.LOW)
+        GPIO.output(19, GPIO.HIGH)
+
 
     while True:
-        return render(request, 'main.html')
-
-
-def about(request):
-
-    while True:
-        return render(request, 'about.html')
+               return render(request,'main.html');
