281111682eb8b172e3b6b8d3c7a5fb5580c46342
Danielle Marie Espiritu <danielleespiritu17@gmail.com>
2019-04-01 20:47:35 +0800

    Added camera

--- blinkled/led/views.py
+++ blinkled/led/views.py
@@ -2,53 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-#!/usr/bin/python
-import sys
-import Adafruit_DHT
-# Create your views here.
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-# LIGHT BULB
-GPIO.setup(21, GPIO.OUT, initial=0)
-# FAN
-GPIO.setup(20, GPIO.OUT, initial=0)
-# DHT11
-sensor = Adafruit_DHT.DHT11
-# SOIL MOISTURE SENSOR
-GPIO.setup(12, GPIO.IN)
-
 
 def main(request):
     if (request.GET.get('onLED_btn')):
-        print("(Pin 21) The light is ON")
-        GPIO.output(21, GPIO.HIGH)
+        print("The light is ON")
 
     if (request.GET.get('offLED_btn')):
-        print("(Pin 21) The light is OFF")
-        GPIO.output(21, GPIO.LOW)
+        print("The light is OFF")
 
     if (request.GET.get('onFAN_btn')):
-        print("(Pin 20) The fan is ON")
-        GPIO.output(20, GPIO.HIGH)
+        print("The fan is ON")
 
     if (request.GET.get('offFAN_btn')):
-        print("(Pin 20) The fan is OFF")
-        GPIO.output(20, GPIO.LOW)
+        print("The fan is OFF")
 
     if (request.GET.get('refresh_btn')):
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, 16)
-        if humidity is not None and temperature is not None:
-            print(
-                'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-        else:
-            print('Failed to get reading. Try again!')
+        print("Page refreshed!")
 
-        print("(Pin 16 for DHT11) Page refreshed!")
-        
-        if GPIO.input(12):
-                print ("Water Detected!")
-        else:
-                print ("No Water Detected!")
 
     while True:
         return render(request, 'main.html')
