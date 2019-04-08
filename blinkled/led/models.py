22b1d739d7421449c4db3d8dacb178ef40c5a364
Daniellee17 <Danielleespiritu17@gmail.com>
2019-04-08 18:34:30 +0800

    minor

--- blinkled/led/models.py
+++ blinkled/led/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class sensors(models.Model):
-    temperature = models.FloatField(max_length=250);
-    moisture = models.FloatField(max_length=250);
-    humidity = models.FloatField(max_length=250);
+    temperature = models.FloatField(max_length=250, default=0.0);
+    moisture = models.FloatField(max_length=250, default=0.0);
+    humidity = models.FloatField(max_length=250, default=0.0);
     fanStatus = models.TextField(max_length=250, default="Deactivated");
     lightStatus = models.TextField(max_length=250, default="Deactivated");
     action = models.TextField(max_length=250, default="None");
